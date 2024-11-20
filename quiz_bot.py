62b8c04a3b7773b8952f073082a93ba726075b68
anzhelikkaa <poruchnikk@gmail.com>
2024-11-20 11:10:28 +0200

    Update quiz_bot.py

--- quiz_bot.py
+++ quiz_bot.py
@@ -325,7 +325,4 @@ def calculate_results(chat_id):
     bot.send_message(chat_id, "Бажаєте спробувати ще раз? Напишіть /start")
 
 if __name__ == '__main__':
-    try:
         bot.polling(none_stop=True)
-    except Exception as e:
-        print(f"Сталася помилка: {e}")
