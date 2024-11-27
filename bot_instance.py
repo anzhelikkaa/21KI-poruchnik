07612c4720327d16d0b88db99e60a9f96ac5e885
anzhelikkaa <poruchnikk@gmail.com>
2024-11-27 03:26:06 +0200

    Add files via upload

---
+++ bot_instance.py
@@ -0,0 +1,3 @@
+import telebot
+
+bot = telebot.TeleBot('7898616545:AAFyxJ-cD0e8Uwk30RzK_dhJgLZYwmecG8U')
---
+++ handlers/topic_handler.py
@@ -0,0 +1,9 @@
+from telebot.types import ReplyKeyboardMarkup
+from bot_instance import bot
+from data.user_data import user_data  # Імпортуємо user_data із data.user_data
+
+def handle_topic_selection(chat_id, selected_topic):
+    user_data[chat_id] = {'selected_topic': selected_topic}
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add("Почати вікторину", "Повернутися до вибору теми")
+    bot.send_message(chat_id, f"Ви вибрали тему: {selected_topic}. Бажаєте почати вікторину?", reply_markup=markup)
