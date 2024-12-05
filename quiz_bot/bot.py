1d08552df2d8d6e601b25810490d3f0fa62a9359
anzhelikkaa <poruchnikk@gmail.com>
2024-12-05 11:24:45 +0200

    Delete quiz_bot directory

--- quiz_bot/bot.py
+++
@@ -1,7 +0,0 @@
-import os
-import telebot
-from dotenv import load_dotenv
-
-load_dotenv()
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-bot = telebot.TeleBot(bot_token)
--- quiz_bot/main.py
+++
@@ -1,9 +0,0 @@
-from bot import bot
-from handlers import register_handlers
-
-def main():
-    register_handlers(bot)
-    bot.polling(none_stop=True)
-
-if __name__ == '__main__':
-    main()
